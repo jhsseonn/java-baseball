9b3f5ccc30a4421e5df994b35ce50df3f8ada8ce
jhsseonn <hs125bt@gmail.com>
2023-09-27 19:53:40 +0900

    style: 게임 시작 멘트 및 숫자 입력 멘트 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,8 +8,14 @@ public class Application {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
         int randomNumber = Randoms.pickNumberInRange(1, 9);
+        System.out.println("숫자를 입력해주세요 : ");
         int inputNumber = sc.nextInt();
+
+
+
 
 
     }
