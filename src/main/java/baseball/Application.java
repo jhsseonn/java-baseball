1489eacb4b1b0c5a664bfe5132dc6fa84e522411
jhsseonn <hs125bt@gmail.com>
2023-09-27 20:21:00 +0900

    feat: 스트라이크가 3일 때 출력문 및 게임 재실행 여부 받기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -44,5 +44,15 @@ public class Application {
             }
         }
 
+        if (strike==3){
+            System.out.println(strike+"스트라이크\n"+
+                    "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
+                    "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+            int startOrEnd = sc.nextInt();
+            if (startOrEnd==1){
+                strike=0;
+            }
+        }
+
     }
 }
