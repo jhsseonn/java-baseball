32d023bfb3cb1e79ef9877528ad1b6e2a72c401c
jhsseonn <hs125bt@gmail.com>
2023-09-27 20:20:26 +0900

    feat: 게임이 스트라이크가 3 미만일 때만 실행되게 설정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,34 +12,36 @@ public class Application {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (strike<3) {
+            System.out.println("숫자 야구 게임을 시작합니다.");
 
-        int randomNumber = Randoms.pickNumberInRange(1, 9);
-        System.out.println("숫자를 입력해주세요 : ");
-        int inputNumber = sc.nextInt();
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            System.out.println("숫자를 입력해주세요 : ");
+            int inputNumber = sc.nextInt();
 
-        String randNumStr = Integer.toString(randomNumber);
-        String inputNumStr = Integer.toString(inputNumber);
+            String randNumStr = Integer.toString(randomNumber);
+            String inputNumStr = Integer.toString(inputNumber);
 
-        for (int i=0; i<3; i++) {
-            int index = 0;
-            Character rn = randNumStr.charAt(i);
-            if (inputNumStr.contains(rn.toString())) {
-                index = inputNumStr.indexOf(rn);
+            for (int i=0; i<3; i++){
+                int index = 0;
+                Character rn = randNumStr.charAt(i);
+                if (inputNumStr.contains(rn.toString())){
+                    index = inputNumStr.indexOf(rn);
+                }
+                if (i==index){
+                    strike+=1;
+                } else {
+                    ball+=1;
+                }
             }
-            if (i == index) {
-                strike += 1;
+
+            if (strike==0){
+                System.out.println(ball+"볼");
+            } else if (ball==0){
+                System.out.println(strike+"스트라이크");
             } else {
-                ball += 1;
+                System.out.println(ball+"볼 "+strike+"스트라이크");
             }
-        }
-
-        if (strike==0){
-            System.out.println(ball+"볼");
-        } else if (ball==0){
-            System.out.println(strike+"스트라이크");
-        } else {
-            System.out.println(ball+"볼 "+strike+"스트라이크");
         }
 
     }
