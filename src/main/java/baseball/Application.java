92156444a35bbc20041c67bcea953f6e2ccb8d83
jhsseonn <hs125bt@gmail.com>
2023-09-27 20:38:15 +0900

    feat: 스트라이크/볼 개수에 따른 출력문에 낫싱 누락 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -13,20 +13,26 @@ public class Application {
         Scanner sc = new Scanner(System.in);
 
         while (strike<3) {
+            strike = 0;
+            ball =0;
             System.out.println("숫자 야구 게임을 시작합니다.");
 
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             System.out.println("숫자를 입력해주세요 : ");
-            int inputNumber = sc.nextInt();
+            String inputNumber = sc.next();
 
             String randNumStr = Integer.toString(randomNumber);
-            String inputNumStr = Integer.toString(inputNumber);
+
+            if (inputNumber.length()!=3){
+                throw new IllegalArgumentException();
+            }
 
             for (int i=0; i<3; i++){
                 int index = 0;
-                Character rn = randNumStr.charAt(i);
-                if (inputNumStr.contains(rn.toString())){
-                    index = inputNumStr.indexOf(rn);
+                String rn = Character.toString(randNumStr.charAt(i));
+                System.out.println(rn);
+                if (inputNumber.contains(rn)){
+                    index = inputNumber.indexOf(rn);
                 }
                 if (i==index){
                     strike+=1;
@@ -39,12 +45,14 @@ public class Application {
                 System.out.println(ball+"볼");
             } else if (ball==0){
                 System.out.println(strike+"스트라이크");
-            } else {
+            } else if (strike==0 && ball==0) {
+                System.out.println("낫싱");
+            }else {
                 System.out.println(ball+"볼 "+strike+"스트라이크");
             }
         }
 
-        if (strike==3){
+        if (strike>=3){
             System.out.println(strike+"스트라이크\n"+
                     "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
                     "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
