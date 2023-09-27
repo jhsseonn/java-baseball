6ea05451d4b4848db252c1817a9e3f650d9c3a95
jhsseonn <hs125bt@gmail.com>
2023-09-27 23:08:24 +0900

    refactor: 게임 재시작 위해 게임 로직 함수 리팩토링

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,12 +5,11 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.Scanner;
 
 public class Application {
-    public static void main(String[] args) {
 
-        int strike = 0;
-        int ball = 0;
+    public static void startBaseball(int strike, int ball, Scanner sc){
 
-        Scanner sc = new Scanner(System.in);
+        strike = 0;
+        ball = 0;
 
         System.out.println("숫자 야구 게임을 시작합니다.");
         String randNumStr = "";
@@ -55,17 +54,25 @@ public class Application {
         }
 
         if (strike>=3){
-            System.out.println(strike+"스트라이크\n"+
-                    "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
                     "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             String startOrEnd = sc.next();
             System.out.println(startOrEnd);
             if (startOrEnd.equals("1")){
-                main(args);
+                startBaseball(strike, ball, sc);
             } else {
                 System.out.println("게임 종료");
             }
         }
+    }
+    public static void main(String[] args) {
+
+        int strike = 0;
+        int ball = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        startBaseball(strike, ball, sc);
 
     }
 }
