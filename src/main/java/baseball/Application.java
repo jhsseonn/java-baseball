9dcd3adb50b494d1d9eadb68711e4b2d53f51807
jhsseonn <hs125bt@gmail.com>
2023-09-27 17:55:47 +0900

    feat: 1~9까지의 서로 다른 임의의 수 3개 선택

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        int randomNumber = Randoms.pickNumberInRange(1, 9);
+
+
     }
 }
