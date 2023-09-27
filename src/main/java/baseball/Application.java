ec720bcd1d98dbea031c8006d564e1923f19aca7
jhsseonn <hs125bt@gmail.com>
2023-09-27 22:54:08 +0900

    fix: 낫싱 출력 안됨 해결

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,16 +12,19 @@ public class Application {
 
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        String randNumStr = "";
+        for (int i=0; i<3; i++){
+            randNumStr+=Integer.toString(Randoms.pickNumberInRange(1, 9));
+        }
+
         while (strike<3) {
             strike = 0;
             ball =0;
-            System.out.println("숫자 야구 게임을 시작합니다.");
 
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
             System.out.println("숫자를 입력해주세요 : ");
             String inputNumber = sc.next();
-
-            String randNumStr = Integer.toString(randomNumber);
+            System.out.println(randNumStr);
 
             if (inputNumber.length()!=3){
                 throw new IllegalArgumentException();
@@ -30,10 +33,9 @@ public class Application {
             for (int i=0; i<3; i++){
                 int index = 0;
                 String rn = Character.toString(randNumStr.charAt(i));
-                System.out.println(rn);
                 if (inputNumber.contains(rn)){
                     index = inputNumber.indexOf(rn);
-                }
+                } else continue;
                 if (i==index){
                     strike+=1;
                 } else {
@@ -41,12 +43,12 @@ public class Application {
                 }
             }
 
-            if (strike==0){
-                System.out.println(ball+"볼");
+            if (strike==0 && ball==0) {
+                System.out.println("낫싱");
             } else if (ball==0){
                 System.out.println(strike+"스트라이크");
-            } else if (strike==0 && ball==0) {
-                System.out.println("낫싱");
+            } else if (strike==0) {
+                System.out.println(ball+"볼");
             }else {
                 System.out.println(ball+"볼 "+strike+"스트라이크");
             }
@@ -55,10 +57,13 @@ public class Application {
         if (strike>=3){
             System.out.println(strike+"스트라이크\n"+
                     "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
-                    "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
-            int startOrEnd = sc.nextInt();
-            if (startOrEnd==1){
-                strike=0;
+                    "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String startOrEnd = sc.next();
+            System.out.println(startOrEnd);
+            if (startOrEnd.equals("1")){
+                main(args);
+            } else {
+                System.out.println("게임 종료");
             }
         }
 
