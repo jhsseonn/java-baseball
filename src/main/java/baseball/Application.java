2af1f5423919609ac320d4bbf4ecaefb216e88c4
jhsseonn <hs125bt@gmail.com>
2023-09-27 20:18:42 +0900

    feat: 임의의 숫자 3개와 입력한 숫자 3개 비교해 스트라이크/볼 개수 세기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,6 +6,10 @@ import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
+
+        int strike = 0;
+        int ball = 0;
+
         Scanner sc = new Scanner(System.in);
 
         System.out.println("숫자 야구 게임을 시작합니다.");
@@ -14,9 +18,21 @@ public class Application {
         System.out.println("숫자를 입력해주세요 : ");
         int inputNumber = sc.nextInt();
 
+        String randNumStr = Integer.toString(randomNumber);
+        String inputNumStr = Integer.toString(inputNumber);
 
-
-
+        for (int i=0; i<3; i++){
+            int index = 0;
+            Character rn = randNumStr.charAt(i);
+            if (inputNumStr.contains(rn.toString())){
+                index = inputNumStr.indexOf(rn);
+            }
+            if (i==index){
+                strike+=1;
+            } else {
+                ball+=1;
+            }
+        }
 
     }
 }
