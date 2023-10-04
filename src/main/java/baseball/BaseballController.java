6d204326b7a9da78c96cfd84983f0ca5e677da34
jhsseonn <hs125bt@gmail.com>
2023-10-04 18:02:03 +0900

    refactor: 기능별 함수 리팩토링(BaseballService)

--- src/main/java/baseball/BaseballController.java
+++ src/main/java/baseball/BaseballController.java
@@ -7,48 +7,20 @@ import java.util.Scanner;
 public class BaseballController {
 
     BaseballOutput baseballOutput = new BaseballOutput();
+    BaseballService baseballService = new BaseballService();
     Scanner sc = new Scanner(System.in);
 
     public void startBaseball(){
 
         int strike = 0;
-        int ball = 0;
 
         baseballOutput.printGameStart();
-        String randNumStr = "";
-        for (int i=0; i<3; i++){
-            randNumStr+=Integer.toString(Randoms.pickNumberInRange(1, 9));
-        }
 
         while (strike<3) {
-            strike = 0;
-            ball =0;
-
-            baseballOutput.printInputNum();
-            String inputNumber = sc.next();
-//            System.out.println(randNumStr);
-
-            if (inputNumber.length()!=3){
-                throw new IllegalArgumentException();
-            }
-
-            for (int i=0; i<3; i++){
-                int index = 0;
-                String rn = Character.toString(randNumStr.charAt(i));
-                if (inputNumber.contains(rn)){
-                    index = inputNumber.indexOf(rn);
-                } else continue;
-                if (i==index){
-                    strike+=1;
-                } else {
-                    ball+=1;
-                }
-            }
-
-            baseballOutput.printResult(strike, ball);
+            strike = baseballService.startGame(baseballService.getRandNum());
         }
 
-        if (strike>=3){
+        if (strike==3){
             baseballOutput.printGameOver();
             String startOrEnd = sc.next();
 //            System.out.println(startOrEnd);
--- src/main/java/baseball/BaseballService.java
+++ src/main/java/baseball/BaseballService.java
@@ -1,6 +1,66 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Scanner;
+
 public class BaseballService {
 
+    BaseballOutput baseballOutput = new BaseballOutput();
+    Scanner sc = new Scanner(System.in);
 
+    public String getRandNum(){
+        String randNumStr = "";
+        for (int i=0; i<3; i++){
+            randNumStr+=Integer.toString(Randoms.pickNumberInRange(1, 9));
+        }
+        return randNumStr;
+    }
+
+    public int getStrike(String randNumStr, String inputNumber, int strike){
+        for (int i=0; i<3; i++){
+            int index = 0;
+            String rn = Character.toString(randNumStr.charAt(i));
+            if (inputNumber.contains(rn)){
+                index = inputNumber.indexOf(rn);
+            } else continue;
+            if (i==index){
+                strike+=1;
+            }
+        }
+        return strike;
+    }
+
+    public int getBall(String randNumStr, String inputNumber, int ball) {
+        for (int i=0; i<3; i++){
+            int index = 0;
+            String rn = Character.toString(randNumStr.charAt(i));
+            if (inputNumber.contains(rn)){
+                index = inputNumber.indexOf(rn);
+            } else continue;
+            if (i!=index){
+                ball+=1;
+            }
+        }
+        return ball;
+    }
+
+    public int startGame(String randNumStr) {
+        int strike = 0;
+        int ball =0;
+
+        baseballOutput.printInputNum();
+        String inputNumber = sc.next();
+
+        if (inputNumber.length()!=3){
+            throw new IllegalArgumentException();
+        }
+
+        strike = getStrike(randNumStr, inputNumber, strike);
+        ball = getBall(randNumStr, inputNumber, ball);
+
+        baseballOutput.printResult(strike, ball);
+
+        return strike;
+    }
 }
